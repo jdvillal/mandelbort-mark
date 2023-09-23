fb7359cba38c11b8fe256ed459b21dbea4ad0a4a
Jorge <daniel.--96@hotmail.com>
2023-09-23 15:56:53 -0500

    fix coloring algorithm

--- mandelbrotMark/src-tauri/src/main.rs
+++ mandelbrotMark/src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+use std::time::Instant;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,41 +12,53 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-fn calc_mandelbrot(a0: f64, b0: f64, af: f64, bf: f64) -> Vec<f64>{
-    let a_step: f64 = (af - a0) / 800.0;
-    let b_step: f64 = (bf - b0) / 800.0;
-    let max_iterations: i32 = 100;
+fn calc_mandelbrot(a0: f64, b0: f64, af: f64, bf: f64, width: usize, height: usize, max_iterations: i32) -> Vec<u8>{
+    //let width = 500;
+    //let height = 500;
+    let now = Instant::now();
+    let a_step: f64 = (af - a0).abs() / width as f64;
+    let b_step: f64 = (bf - b0).abs() / height as f64;
+    println!("x_step {:?}", a_step);
+    println!("y_step {:?}", b_step);
+    //let max_iterations: i32 = 10000;
 
-    let mut result: Vec<f64> = Vec::new();
-    for i in 0..800{
-        let ai: f64 = a0 + i as f64 * a_step;
-        for j in 0..800{
-            let bj: f64 = b0 + j as f64 * b_step;
-
-            let mut a: f64 = 0.0;
-            let mut b: f64 = 0.0;
-
-            let mut iteration: i32 = 0;
+    let mut result: Vec<u8> = Vec::with_capacity(width*height*3);
+    for i in 0..height{
+        let py: f64 = b0 + (i as f64 * b_step);
+        //println!("px =================>{:?}", px);
+        for j in 0..width{
             
-            let mut a2: f64 = 0.0;
-            let mut b2: f64 = 0.0;
+            let px: f64 = a0 + (j as f64 * a_step);
+            //println!("py ==> {}", py);
+            //println!("x,y = {},{}", i, j);
+            let mut iteration: i32 = 0;
 
-            while a2+b2 <= 4.0 && iteration < max_iterations {
-                b = 2.0 * a * b + bj;
-                a = a2 - b2 + ai;
-                a2 = a * a;
-                b2 = b * b;
+            let mut x = 0.0;
+            let mut y = 0.0;
+            
+            let mut x2 = 0f64;
+            let mut y2 = 0f64;
+
+            while x2 + y2 <= 4f64 && iteration < max_iterations {
+                y = 2f64 * x * y + py;
+                x = x2 - y2 + px;
+                x2 = x * x;
+                y2 = y * y;
                 iteration = iteration + 1;
             }
             let n = iteration as f64;
-            let red: f64 = 0.5 * (a*n).sin() + 0.5;
-            let green = 0.5 * (a * n + 2.094).sin() + 0.5;
-            let blue = 0.5 * (a * n + 4.188).sin() + 0.5;
-            result.push(red);
-            result.push(green);
-            result.push(blue);
+            let red: f64 = 0.5 * (0.1 * n).sin() + 0.5;
+            let green = 0.5 * (0.1 * n + 2.094).sin() + 0.5;
+            let blue = 0.5 * (0.1 * n + 4.188).sin() + 0.5;
+            
+
+            result.push((red * 255 as f64) as u8);
+            result.push((green * 255 as f64) as u8);
+            result.push((blue * 255 as f64) as u8);
+            //result.push(255);
         }
     }
+    println!("{}", now.elapsed().as_millis());
     result
 }
 
--- mandelbrotMark/src/main.js
+++ mandelbrotMark/src/main.js
@@ -3,24 +3,59 @@ const { invoke } = window.__TAURI__.tauri;
 let greetInputEl;
 let greetMsgEl;
 
+let c_width = 800;
+let c_height = 800;
+
+
+async function draw_fractal2(){
+  var x = Math.floor(Math.random() * canvasWidth);
+  var y = Math.floor(Math.random() * canvasHeight);
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  var off = (y * id.width + x) * 4;
+  pixels[off] = r;
+  pixels[off + 1] = g;
+  pixels[off + 2] = b;
+  pixels[off + 3] = 255;
+  console.log("flag");
+  ctx.putImageData(id, 0, 0);
+}
+function draw_fractal(result, ctx, imageData){
+  let off = 0;
+  let lenght = c_width*c_height*3;
+  for(let i = 0; i < lenght; i+=3){
+    imageData.data[i+off] = result[i];
+    imageData.data[i+off + 1] = result[i+1];
+    imageData.data[i+off + 2] = result[i+2];
+    imageData.data[i+off + 3] = 255;
+    off = off + 1;
+  }
+  console.log(imageData)
+  ctx.putImageData(imageData, 0, 0);
+}
+
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
 
-async function calc_maldelbrot(){
-  let result = await invoke("calc_mandelbrot",{a0: -2, b0: 2, af: 2, bf: -2, max_iterations: 100});
-  console.log(result);
-  //calc_mandelbrot(a0: f64, b0: f64, af: f64, bf: f64, a_dim: i32, b_dim: i32, max_iterations: i32)
-}
-
 window.addEventListener("DOMContentLoaded", () => {
-
+  
   let fractal_canvas = document.getElementById("fractal-canvas");
 
+  let canvas = document.getElementById('fractal-canvas');
+  let ctx = canvas.getContext('2d');
+  let canvasWidth = canvas.width;
+  let canvasHeight = canvas.height;
+  console.log(canvasWidth, canvasHeight);
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  //let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+  let imageData = ctx.createImageData(c_width,c_height);
   document.getElementById('calc-btn').addEventListener('click', async ()=>{
-    let result = await invoke("calc_mandelbrot",{a0: -2, b0: 2, af: 2, bf: -2, aDim: 800, bDim: 800, maxIterations: 100});
-    console.log(result);
+    let result = await invoke("calc_mandelbrot",{a0: -2.5, b0: -2, af:1.5, bf: 2, width: c_width, height: c_height, maxIterations: 500});
+    console.log(result.lenght, result);
+    draw_fractal(result, ctx, imageData);
   });
 
 
